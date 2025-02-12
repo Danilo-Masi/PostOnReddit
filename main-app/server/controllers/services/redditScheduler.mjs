ddff0297aadb751c382cf3771f976b647ae08cb2
dmasiii <danilomasi999@gmail.com>
2025-02-12 17:30:46 +0100

    postonreddit

--- main-app/server/controllers/services/redditScheduler.mjs
+++ main-app/server/controllers/services/redditScheduler.mjs
@@ -1,18 +1,29 @@
 import supabase from '../../config/supabase.mjs';
 import logger from '../../config/logger.mjs';
 import { submitPostToReddit } from './redditServices.mjs';
+import moment from 'moment-timezone';
 
 export const scheduleRedditPosts = async () => {
-    // Crea un timestamp UTC nel formato 'YYYY-MM-DD HH:MI:SS+00'
-    const nowUtc = new Date();
+
+    const receivedTimeInItalianTimeZone = moment().tz('Europe/Rome'); 
+
+    const nowUtc = receivedTimeInItalianTimeZone.utc().format('YYYY-MM-DD HH:mm:ss+00'); 
+
     nowUtc.setSeconds(0, 0);
-    const nowUtcFormatted = nowUtc.toISOString().replace('T', ' ').slice(0, 19) + '+00';
+    
+    logger.info(`Orario corrente in Europe/Rome: ${nowUtc}`);
+    logger.info(`Orario corrente in UTC: ${nowUtc}`);
+
+    // Crea un timestamp UTC nel formato 'YYYY-MM-DD HH:MI:SS+00'
+    /*const nowUtc = new Date();
+    nowUtc.setSeconds(0, 0);
+    const nowUtcFormatted = nowUtc.toISOString().replace('T', ' ').slice(0, 19) + '+00';*/
 
     let { data, error } = await supabase
         .from('posts')
         .select('*')
         .eq('status', 'pending')
-        .eq('date_time', nowUtcFormatted)
+        .eq('date_time', nowUtc)
 
     if (error) {
         logger.error('Errore nel recupero dei post da caricare dal DB', error.message);
