3c1b3579e4f665415facb61e968acf5682511370
dmasiii <danilomasi999@gmail.com>
2025-02-12 16:47:15 +0100

    postonreddit

--- main-app/server/controllers/services/redditScheduler.mjs
+++ main-app/server/controllers/services/redditScheduler.mjs
@@ -6,19 +6,13 @@ export const scheduleRedditPosts = async () => {
     // Crea un timestamp UTC nel formato 'YYYY-MM-DD HH:MI:SS+00'
     const nowUtc = new Date();
     nowUtc.setSeconds(0, 0);
-
-    const oneMinuteLater = new Date(nowUtc.getTime() + 60000);
-
-    // Converte il timestamp in formato compatibile con il database
     const nowUtcFormatted = nowUtc.toISOString().replace('T', ' ').slice(0, 19) + '+00';
-    const oneMinuteLaterFormatted = oneMinuteLater.toISOString().replace('T', ' ').slice(0, 19) + '+00';
 
     let { data, error } = await supabase
         .from('posts')
         .select('*')
         .eq('status', 'pending')
-        .gte('date_time', nowUtcFormatted)
-        .lt('date_time', oneMinuteLaterFormatted);
+        .eq('date_time', nowUtcFormatted)
 
     if (error) {
         logger.error('Errore nel recupero dei post da caricare dal DB', error.message);
@@ -26,9 +20,7 @@ export const scheduleRedditPosts = async () => {
     }
 
     if (data.length === 0) {
-        logger.info(`Post recuperati tra ${nowUtcFormatted} e ${oneMinuteLaterFormatted}: []`);
-    } else {
-        logger.info(`Post recuperati tra ${nowUtcFormatted} e ${oneMinuteLaterFormatted}: ${JSON.stringify(data)}`);
+        logger.info(`Post recuperati alle ${nowUtcFormatted}}: []`);
     }
 
     for (let post of data) {
